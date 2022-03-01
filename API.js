18a08f4cc26735f0521e997ba17c7253201edc9d
Sadia-90 <sadiamahmood24@gmail.com>
2022-03-01 14:28:25 +0600

    5th

--- API.js
+++ API.js
@@ -13,20 +13,41 @@ const displayPhones = (data) => {
 
   data.forEach((phoneItem) => {
     console.log(phoneItem)
-
     const parent = document.getElementById("Phone-container");
-    const div = document.createElement('div');
+    const div = document.createElement("div");
     div.innerHTML = `
-    <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
+    <div class="card" style="width: 18rem;">
+<img src="${phoneItem.image}" class="card-img-top" alt="...">
+<div class="card-body">
+    <h3 class="card-title">${phoneItem.phone_name}</h3>
+    <h5>Category: ${phoneItem.brand}</h5>
+   
+    <button onclick="information('${phoneItem.slug}')" type="button" id="info-btn" class="btn btn-primary">See Details</button>
+</div>
+</div>
     `;
     parent.appendChild(div);
-  });
+    console.log(phoneItem)
+  })
+};
+// info detail
+
+const information = (id) => {
+  // const detailsDiv = document.getElementById('details-div');
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then((data) => displayId(data.data));
+}
+
+const displayId = (data) => {
+  const detailsDiv = document.getElementById('details-div').innerHTML;
+  const div = document.createElement("div");
+  div.innerHTML =`
+  <div>
+  <img src="..." alt="...">
+  <h1>Name: </h1>
+  <h3>release date: </h3>
+  `
 }
